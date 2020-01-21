0bb5bdef67135e95e6c41b488b2b87ced1b368fe
paul <34345790@qq.com>
2020-01-21 18:45:07 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-import Vue from 'vue/dist/vue.esm'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-	// render: h => h(App),
-	template: `<App style="margin: 100px"><div style="width: 300px;height: 150px;"></div></App>`,
-	components: {App}
-}).$mount('#app')
+export { default } from './App.vue'
